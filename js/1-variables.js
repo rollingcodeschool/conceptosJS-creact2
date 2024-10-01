0b7f95ccde60314747b54300f4abe7ea9a3d1b47
Emilse Arias <emilsearias@gmail.com>
2024-10-01 11:34:09 -0300

    mostramos un mensaje en el documento html

--- js/1-variables.js
+++ js/1-variables.js
@@ -1,5 +1,5 @@
 //mensaje que muestro en consola
-console.log('Hola mundo')
+console.log("Hola mundo");
 
 /* comentario en 
 multiples
@@ -11,14 +11,20 @@ let num1;
 
 //declarar e inicializar una variable
 let num2 = 20;
-const pi = 3.14
-let nombre = 'emi'
+const pi = 3.14;
+let nombre = "emi";
 //asignar un valor a una variable
 num1 = 10;
 //mostrar por consola el valor de las variables
-console.log('variable num1='+ num1+' variable num2='+num2)
-console.log('variable pi='+ pi)
+console.log("variable num1=" + num1 + " variable num2=" + num2);
+console.log("variable pi=" + pi);
 
-console.log('mensaje de prueba')
-console.log('variable nombre='+ nombre)
+console.log("mensaje de prueba");
+console.log("variable nombre=" + nombre);
 
+//mostrar el mensaje en el documento HTML
+document.write("La suma de los dos números es: " + (num1 + num2));
+document.write("<p>La suma de los dos números es: " + (num1 + num2) + "</p>");
+//ejercio 1
+
+//ejercicio 2
