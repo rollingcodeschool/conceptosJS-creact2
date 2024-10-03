d4a4444091601317efba61db33401aa413a5db8c
Emilse Arias <emilsearias@gmail.com>
2024-10-03 11:47:17 -0300

    agregamos un else if en nuestro programa de elecciones

--- js/2-condicionales.js
+++ js/2-condicionales.js
@@ -7,7 +7,7 @@ if(condicion logica){
 // const numero = parseInt(prompt("ingrese un número"));
 
 // if(numero % 2 === 0){
-//     document.write('El numero ingresado: '+ numero + ' es par')   
+//     document.write('El numero ingresado: '+ numero + ' es par')
 // }else{
 //     document.write(`El numero ingresado: ${numero} es impar`)
 // }
@@ -18,21 +18,18 @@ if(condicion logica){
 //16-17 o mayores de 70: puedes votar (optativo)
 //18-70: Estoy obligado a votar
 
-const edad = parseInt(prompt('Ingrese su edad'));
+const edad = parseInt(prompt("Ingrese su edad"));
 
-if( edad >= 1 && edad<= 105 ){
-    //aqui continuo con el programa
-    console.log('aqui sigo con mi programa')
-    if( (edad>=16 && edad <=17) || edad > 70 ){
-        document.write('Es optativo que votes')
-    }else{
-        if(edad >= 18 && edad <= 70){
-            document.write('Es obligatorio votar')
-        }else{
-            document.write('No peudes votar')
-        }
-    }
-
-}else{
-    alert('Ingresaste un valor invalido')
+if (edad >= 1 && edad <= 105) {
+  //aqui continuo con el programa
+  console.log("aqui sigo con mi programa");
+  if ((edad >= 16 && edad <= 17) || edad > 70) {
+    document.write("Es optativo que votes");
+  } else if (edad >= 18 && edad <= 70) {
+    document.write("Es obligatorio votar");
+  } else {
+    document.write("No peudes votar");
+  }
+} else {
+  alert("Ingresaste un valor invalido");
 }
