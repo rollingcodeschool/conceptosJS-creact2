e9de1090e368e745378127a510b89b28385cd588
Emilse Arias <emilsearias@gmail.com>
2024-10-03 11:28:02 -0300

    completamos las condiciones logicas para el ejemplo de las elecciones

--- js/2-condicionales.js
+++ js/2-condicionales.js
@@ -1,15 +1,38 @@
 /*
 if(condicion logica){
     todas las lineas de codigo que quiero si cumple la condicion
-} else {
- todas las lineas de codigo que quiero si no se cumple la condicion
-}
+} 
 */
 
-const numero = parseInt(prompt("ingrese un número"));
+// const numero = parseInt(prompt("ingrese un número"));
 
-if(numero % 2 === 0){
-    document.write('El numero ingresado: '+ numero + ' es par')   
+// if(numero % 2 === 0){
+//     document.write('El numero ingresado: '+ numero + ' es par')   
+// }else{
+//     document.write(`El numero ingresado: ${numero} es impar`)
+// }
+
+//Ejercicio 2
+
+//Solicitar al usuario su edad e informar si esta obligado a votar o no
+//16-17 o mayores de 70: puedes votar (optativo)
+//18-70: Estoy obligado a votar
+
+const edad = parseInt(prompt('Ingrese su edad'));
+
+if( edad >= 1 && edad<= 105 ){
+    //aqui continuo con el programa
+    console.log('aqui sigo con mi programa')
+    if( (edad>=16 && edad <=17) || edad > 70 ){
+        document.write('Es optativo que votes')
+    }else{
+        if(edad >= 18 && edad <= 70){
+            document.write('Es obligatorio votar')
+        }else{
+            document.write('No peudes votar')
+        }
+    }
+
 }else{
-    document.write(`El numero ingresado: ${numero} es impar`)
+    alert('Ingresaste un valor invalido')
 }
