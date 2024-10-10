88a1322056b3e2f1676604fde4866a288e69ae89
Emilse Arias <emilsearias@gmail.com>
2024-10-10 11:37:33 -0300

    dibujamos la cantidad de columnas que indico el usuario y agregamos el valor de la celda

--- js/extras/tp2-ejercicio10.js
+++ js/extras/tp2-ejercicio10.js
@@ -1,15 +1,17 @@
 // 10- Realiza un script que pida número de filas y columnas y escriba una tabla. Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1
 const filas = parseInt(prompt('Ingrese una cantidad de filas'))
 const columnas = parseInt(prompt('Ingrese una cantidad de columnas'))
-
+let celda = filas * columnas
 document.write(`<table>
         <tbody>`)
 
 for(let indiceFilas = 1; indiceFilas <= filas; indiceFilas++) {
-    document.write(`
-                <tr>
-                    <td>35</td>
-                </tr>`)
+    document.write(`<tr>`)
+    for(let indiceColumnas=1; indiceColumnas<=columnas; indiceColumnas++ ){
+        document.write(`<td>${celda}</td>`)
+        celda--; //celda = celda - 1
+    }
+    document.write(`</tr>`)
 }       
 
 document.write(`
