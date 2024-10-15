3a580d1145c1d1229df9e448d5350dfa59fc0159
Emilse Arias <emilsearias@gmail.com>
2024-10-15 11:30:15 -0300

    modificamos un elemento del array

--- js/5-arrays.js
+++ js/5-arrays.js
@@ -24,3 +24,42 @@ for(let i=0; i < peliculas.length; i++){
     document.write(`<li>${peliculas[i]}</li>`)
 }
 document.write('</ul>')
+
+//agregar elementos en el array
+//agregar un elemento al principio del array
+peliculas.unshift('Interestelar','Los increibles')
+document.write(`<h2>Agregamos 2 elementos al principio del array, peliculas: ${peliculas.length}</h2>`)
+document.write('<ul>')
+for(let i=0; i < peliculas.length; i++){
+    document.write(`<li>${peliculas[i]}</li>`)
+}
+document.write('</ul>')
+
+//agregar un elemento al final del array
+peliculas.push('furia de titanes')
+document.write(`<h2>Agregamos 1 elemento al final del array, peliculas: ${peliculas.length}</h2>`)
+document.write('<ul>')
+for(let i=0; i < peliculas.length; i++){
+    document.write(`<li>${peliculas[i]}</li>`)
+}
+document.write('</ul>')
+
+peliculas.splice(3,0,'spiderman no way home')
+document.write(`<h2>Agregamos 1 elemento en el medio del array, peliculas: ${peliculas.length}</h2>`)
+document.write('<ul>')
+for(let i=0; i < peliculas.length; i++){
+    document.write(`<li>${peliculas[i]}</li>`)
+}
+document.write('</ul>')
+
+//modificar elementos en el array
+peliculas[4] = 'Cars 1'
+document.write(`<h2>Modificamos el elemento de la posicion 4 del array, peliculas: ${peliculas.length}</h2>`)
+document.write('<ul>')
+for(let i=0; i < peliculas.length; i++){
+    document.write(`<li>${peliculas[i]}</li>`)
+}
+document.write('</ul>')
+
+
+//borrar elementos en el array
